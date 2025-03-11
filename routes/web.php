839351dc38b0e73ec3f5d2d6bffd29d645061152
ahalim260187 <raffahaitami@gmail.com>
2025-03-11 16:17:42 +0800

    Pindah closure route ke controller

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\FileUploudController;
+use App\Http\Controllers\JobController;
 use App\Http\Controllers\TestingController;
 use App\Models\JobListing;
 use Illuminate\Http\Request;
@@ -13,66 +14,17 @@ Route::get('/', function () {
     return view('home', ['greeting' => 'Hello']);
 });
 
-// All Jobs
-Route::get('/job', function () {
-    $jobs = JobListing::with('user')->latest()->simplePaginate(5);
-    return view('jobs', compact('jobs'));
-})->name('jobs');
-
-// Create Job
-Route::get('/job/create', function () {
-    return view('create-job');
-});
-
-// Save Job
-Route::post('/job', function (Request $request) {
-    $request->validate([
-        'job_name' => ['string', 'min:3'],
-        'salary' => ['min:5']
-    ]);
-    JobListing::create([
-        'name' => $request->job_name,
-        'salary' => $request->salary,
-        'user_id' => 2
-    ]);
-    return redirect('/job')->with('success', 'Job Saved successfully!');
-});
-
-// Edit Job
-Route::get('/job/{job}/edit', function (JobListing $job) {
-    return view('edit-job', compact('job'));
-})->name('edit-job');
-
-// Update Job
-Route::patch('/job/{job}', function (Request $request, JobListing $job) {
-    $request->validate([
-        'job_name' => ['string', 'min:3'],
-        'salary' => ['min:5']
-    ]);
-    $job->update([
-        'name' => $request->job_name,
-        'salary' => $request->salary,
-    ]);
-    return redirect('/job')->with('success', 'Job updated successfully!');
-})->name('update-job');
-
-// Delete Job
-Route::delete('/job/{job}', function (JobListing $job) {
-    $job->delete();
-    return redirect('/job')->with('success', 'Job Deleted successfully!');
-})->name('delete-job');
-
-// Get Single Job
-Route::get('/job/{job}', function (JobListing $job) {
-    return view('job', ['job' => $job]);
-})->name('jobs.show');
+//Jobs Route
+Route::get('/job', [JobController::class, 'index'])->name('jobs');
+Route::get('/job/create', [JobController::class, 'create']);
+Route::post('/job', [JobController::class, 'store']);
+Route::get('/job/{job}/edit', [JobController::class, 'edit'])->name('edit-job');
+Route::patch('/job/{job}', [JobController::class, 'update'])->name('update-job');
+Route::delete('/job/{job}', [JobController::class, 'delete'])->name('delete-job');
+Route::get('/job/{job}', [JobController::class, 'show'])->name('jobs.show');
 
 Route::get('/contact', function () {
     return view('contact');
-});
-
-Route::get('/team', function () {
-    return view('teams');
 });
 
 Route::get('/add-user', [TestingController::class, 'index']);
